59ac459741470917ea39fff1a808a57f6f044e55
Sterling Kohel <sterling.kohel@gmail.com>
2019-02-26 23:46:21 -0600

    initial

---
+++ src/main/java/Application.java
@@ -0,0 +1,41 @@
+import org.graalvm.polyglot.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+class Application {
+    public static void main(String[] args) throws IOException {
+        String[] supportedLangs = { "js", "python", "R" };
+        Context context = Context.newBuilder(supportedLangs).allowAllAccess(true).build();
+
+        String jsFile = readFile("/home/sterling/IdeaProjects/kohel/src/main/scripts/helloWorld.js");
+        runScript(jsFile, context, "js");
+
+        String pyFile = readFile("/home/sterling/IdeaProjects/kohel/src/main/scripts/helloWorld.py");
+        runScript(pyFile, context, "python");
+
+        String rFile = readFile("/home/sterling/IdeaProjects/kohel/src/main/scripts/helloWorld.R");
+        runScript(rFile, context, "R");
+    }
+
+    private static Value runScript(String script, Context context, String language) {
+        return context.eval(language, script);
+    }
+
+    private static String readFile(String pathname) throws IOException {
+        File file = new File(pathname);
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+        Scanner scanner = new Scanner(file);
+        String lineSeparator = System.getProperty("line.separator");
+
+        try {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine() + lineSeparator);
+            }
+            return fileContents.toString();
+        } finally {
+            scanner.close();
+        }
+    }
+}
